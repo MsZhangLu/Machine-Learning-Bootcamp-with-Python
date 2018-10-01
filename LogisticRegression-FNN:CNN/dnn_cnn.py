f030ce84b0167507fc8f8e08efa67e199e181c7d
Lu <zhan716@usc.edu>
2018-10-01 12:26:48 -0700

    updated folder names

--- LogisticRegression-FNN:CNN/dnn_cnn.py
+++ LogisticRegression-FNN:CNN/dnn_cnn.py
@@ -1,9 +1,6 @@
 """
-Do not change other parts than those marked by TODO.
+Do not change the input and output format.
 If our script cannot run your code or the format is improper, your code will not be graded.
-
-Search TODO for those parts you need to complete.
-Please follow the step indicated in TODO (step) to complete this script from step = 1 to step = 5.
 """
 
 import json
@@ -56,11 +53,6 @@ class DataSplit:
 
 def main(main_params):
 
-    """
-        Search TODO for those parts you need to complete.
-        Please follow the step indicated in TODO (step) to complete this script from step = 1 to step = 5.
-    """
-
     ### set the random seed ###
     np.random.seed(int(main_params['random_seed']))
 
@@ -74,13 +66,13 @@ def main(main_params):
 
     ### building/defining CNN ###
     """
-        In this script, we are going to build a CNN for a 10-class classification problem on MNIST.
-        The network structure is input --> convolution --> relu --> max pooling --> convolution --> relu --> max pooling --> flatten --> dropout --> linear --> softmax_cross_entropy loss
-        the hidden_layer size (num_L1) is 1225
-        the output_layer size (num_L2) is 10
+    In this script, we are going to build a CNN for a 10-class classification problem on MNIST.
+    The network structure is input --> convolution --> relu --> max pooling --> flatten --> dropout --> linear --> softmax_cross_entropy loss
+    the hidden_layer size (num_L1) is 4900
+    the output_layer size (num_L2) is 10
     """
     model = dict()
-    num_L1 = 1225
+    num_L1 = 4900
     num_L2 = 10
 
     # experimental setup
@@ -89,7 +81,7 @@ def main(main_params):
 
     # optimization setting: _alpha for momentum, _lambda for weight decay
     _learning_rate = float(main_params['learning_rate'])
-    _step = 30
+    _step = 10
     _alpha = float(main_params['alpha'])
     _lambda = float(main_params['lambda'])
     _dropout_rate = float(main_params['dropout_rate'])
@@ -98,20 +90,6 @@ def main(main_params):
     model['C1'] = dnn_misc.conv_layer(num_input = d, num_output = 25, filter_len = 5, stride = 1)
     model['nonlinear1'] = dnn_misc.relu()
     model['M1'] = dnn_misc.max_pool(max_len = 2, stride = 2)
-
-    ################################################################################
-    # TODO (1): Understand the new modules to be included (compared to dnn_cnn.py) #
-    # You do not need to modify any thing here.                                    #
-    ################################################################################
-
-    model['C2'] = dnn_misc.conv_layer(num_input = 25, num_output=25, filter_len=3, stride=1)
-    model['nonlinear2'] = dnn_misc.relu()
-    model['M2'] = dnn_misc.max_pool(max_len=2, stride=2)
-
-    ################################################################################
-    #                            End of TODO (1)                                   #
-    ################################################################################
-
     model['F1'] = dnn_misc.flatten_layer()
     model['drop1'] = dnn_misc.dropout(r = _dropout_rate)
     model['L1'] = dnn_misc.linear_layer(input_D = num_L1, output_D = num_L2)
@@ -150,27 +128,7 @@ def main(main_params):
             c1 = model['C1'].forward(x)
             h1 = model['nonlinear1'].forward(c1)
             m1 = model['M1'].forward(h1)
-
-            ################################################################################
-            # TODO (2): Connect the three modules for the forward pass                     #
-            # model['C2']                                                                  #
-            # model['nonlinear2']                                                          #
-            # model['M2']                                                                  #
-            # into the forward pass.                                                       #
-            # Please make sure to connect them with m1 and m2, the input and output of the #
-            # previous and the next modules, respectively                                  #
-            ################################################################################
-
-            # TODO (2)
-            c2 = model['C2'].forward(m1)
-            h2 = model['nonlinear2'].forward(c2)
-            m2 = model['M2'].forward(h2)
-
-            ################################################################################
-            #                            End of TODO (2)                                   #
-            ################################################################################
-
-            f1 = model['F1'].forward(m2)
+            f1 = model['F1'].forward(m1)
             d1 = model['drop1'].forward(f1, is_train = True)
             a1 = model['L1'].forward(d1)
             loss = model['loss'].forward(a1, y)
@@ -179,28 +137,7 @@ def main(main_params):
             grad_a1 = model['loss'].backward(a1, y)
             grad_d1 = model['L1'].backward(d1, grad_a1)
             grad_f1 = model['drop1'].backward(f1, grad_d1)
-            grad_m2 = model['F1'].backward(m2, grad_f1)
-
-            ################################################################################
-            # TODO (3): Connect the three modules for the backward pass                    #
-            # model['C2']                                                                  #
-            # model['nonlinear2']                                                          #
-            # model['M2']                                                                  #
-            # into the backward pass.                                                      #
-            # Please make sure to connect them with grad_m2 and grad_m1, the input and     #
-            # output of the previous and the next modules, respectively                    #
-            # Please pay attention to the number of arguments in the backward pass.        #
-            ################################################################################
-
-            # TODO (3)
-            grad_h2 = model['M2'].backward(h2, grad_m2)
-            grad_c2 = model['nonlinear2'].backward(c2, grad_h2)
-            grad_m1 = model['C2'].backward(m1, grad_c2)
-
-            ################################################################################
-            #                            End of TODO (3)                                   #
-            ################################################################################
-
+            grad_m1 = model['F1'].backward(m1, grad_f1)
             grad_h1 = model['M1'].backward(h1, grad_m1)
             grad_c1 = model['nonlinear1'].backward(c1, grad_h1)
             grad_x = model['C1'].backward(x, grad_c1)
@@ -229,27 +166,7 @@ def main(main_params):
             c1 = model['C1'].forward(x)
             h1 = model['nonlinear1'].forward(c1)
             m1 = model['M1'].forward(h1)
-
-            ################################################################################
-            # TODO (4): Connect the three modules for the forward pass                     #
-            # model['C2']                                                                  #
-            # model['nonlinear2']                                                          #
-            # model['M2']                                                                  #
-            # into the forward pass.                                                       #
-            # Please make sure to connect them with m1 and m2, the input and output of the #
-            # previous and the next modules, respectively                                  #
-            ################################################################################
-
-            # TODO (4)
-            c2 = model['C2'].forward(m1)
-            h2 = model['nonlinear2'].forward(c2)
-            m2 = model['M2'].forward(h2)
-
-            ################################################################################
-            #                            End of TODO (4)                                   #
-            ################################################################################
-
-            f1 = model['F1'].forward(m2)
+            f1 = model['F1'].forward(m1)
             d1 = model['drop1'].forward(f1, is_train = False)
             a1 = model['L1'].forward(d1)
             loss = model['loss'].forward(a1, y)
@@ -273,27 +190,7 @@ def main(main_params):
             c1 = model['C1'].forward(x)
             h1 = model['nonlinear1'].forward(c1)
             m1 = model['M1'].forward(h1)
-
-            ################################################################################
-            # TODO (5): Connect the three modules for the forward pass                     #
-            # model['C2']                                                                  #
-            # model['nonlinear2']                                                          #
-            # model['M2']                                                                  #
-            # into the forward pass.                                                       #
-            # Please make sure to connect them with m1 and m2, the input and output of the #
-            # previous and the next modules, respectively                                  #
-            ################################################################################
-
-            # TODO (5)
-            c2 = model['C2'].forward(m1)
-            h2 = model['nonlinear2'].forward(c2)
-            m2 = model['M2'].forward(h2)
-
-            ################################################################################
-            #                            End of TODO (5)                                   #
-            ################################################################################
-
-            f1 = model['F1'].forward(m2)
+            f1 = model['F1'].forward(m1)
             d1 = model['drop1'].forward(f1, is_train = False)
             a1 = model['L1'].forward(d1)
             val_acc += np.sum(predict_label(a1) == y)
@@ -306,7 +203,7 @@ def main(main_params):
 
     # save file
     json.dump({'train': train_acc_record, 'val': val_acc_record},
-              open('CNN2_lr' + str(main_params['learning_rate']) +
+              open('CNN_lr' + str(main_params['learning_rate']) +
                    '_m' + str(main_params['alpha']) +
                    '_w' + str(main_params['lambda']) +
                    '_d' + str(main_params['dropout_rate']) +
@@ -320,7 +217,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--random_seed', default=2)
-    parser.add_argument('--learning_rate', default=0.001)
+    parser.add_argument('--learning_rate', default=0.01)
     parser.add_argument('--alpha', default=0.0)
     parser.add_argument('--lambda', default=0.0)
     parser.add_argument('--dropout_rate', default=0.5)
